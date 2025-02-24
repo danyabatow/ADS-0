aba1fcb4236c2342ee4cd0bbdc3f7b21dff01958
danyabatow <danya.batov06@mail.ru>
2025-02-24 21:46:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,10 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-
+  int min = a > b ? b : a;
+  for(int i = min; i > 0; i--) {
+      if(a % i == 0 && b % i == 0) {
+         return i;
+      }
+  }
 }
