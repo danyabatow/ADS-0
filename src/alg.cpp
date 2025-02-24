179bbb609a0f83f434893a88f8cc8bda68bd2184
danyabatow <danya.batov06@mail.ru>
2025-02-24 22:27:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,9 +2,9 @@
 
 int gcd(int a, int b) {
   int min = a > b ? b : a;
-  for(int i = min; i > 0; i--) {
-      if(a % i == 0 && b % i == 0) {
-         return i;
-      }
+  for (int i = min; i > 0; i--) {
+      if (a % i == 0 && b % i == 0) {
+      return i;
+    }
   }
 }
